7783179a4a81bf14622426039f616e736d6a51c1
Nayan Deep <97220336+nayandeep20028840@users.noreply.github.com>
2024-07-09 19:29:57 +0530

    commits

--- textutils/src/App.jsx
+++ textutils/src/App.jsx
@@ -1,67 +1,63 @@
-import './App.css';
-import React,{ useState } from 'react';
-import About from './components/About';
-import Alert from './components/Alert';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Routes
-} from 'react-router-dom';
-
+import "./App.css";
+import React, { useState } from "react";
+import About from "./components/About";
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
-  // setTimeout(() => {
-  //   document.title = 'TextUtils - Home';
-  // }, 2000);
-  // setTimeout(() => {
-  //   document.title = 'TextUtils - Analyzing your text';
-  // }, 1500);
+  setTimeout(() => {
+    document.title = 'TextUtils - Home';
+  }, 2000);
+  setTimeout(() => {
+    document.title = 'Analyzing your text';
+  }, 1500);
 
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
-    })
+      msg: message,
+      type: type,
+    });
     setTimeout(() => {
       setAlert(null);
     }, 2500);
-  }
-  const toggleMode = ()=>{
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor='grey';
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
       showAlert(" Dark mode has been enabled", "success");
-      document.title = 'TextUtils - Dark Mode';
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor='white';
+      document.title = "TextUtils - Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       showAlert(" Light mode has been enabled", "success");
-      document.title = 'TextUtils - Light Mode';
+      document.title = "TextUtils - Light Mode";
     }
-  }
+  };
   return (
     <>
-    {/* <Router> */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert}/>
-      <div className='container my 3'>
+      <Alert alert={alert} />
+      <div className="container my 3">
         <Routes>
-          <Route path="/about">
-            <About/>
-          </Route>
-          <Route path="/">
-            <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-          </Route>
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/"
+            element={
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter the text to analyze below"
+                mode={mode}
+              />
+            }
+          />
         </Routes>
       </div>
-    {/* </Router> */}
     </>
   );
 }
--- textutils/src/main.jsx
+++ textutils/src/main.jsx
@@ -2,19 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
 import {
   BrowserRouter as Router,
   Route,
-  // Switch,
   Routes
 } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-    <Routes>
-      <Route path="/App">
-        <App />
-      </Route>
-    </Routes>
+    <App />
   </Router>
 )
